bf59396fbe3d386c3354a0174cadcc891f51c9fe
EduardoSPorto <eduardosamporto@gmail.com>
2023-08-19 00:45:21 -0300

    "Multiplicação adequada a método atual -Acompanha um input para testes "

--- main2.c
+++ main2.c
@@ -8,7 +8,7 @@ int** matrix_create_100mb( void );
 void matrix_destroy( int** matrix, int sizeX );
 void matrix_print( int** m, int sizeX, int sizeY );
 void matrix_table( int **matrix, int sizeX, int sizeY );
-//Matrix* matrix_multiply( Matrix* m, Matrix* n );
+int** matrix_multiply( int** m, int** n , int nColluns , int mLinhas , int collumMLineN);
 void matrix_internal_add_node( int **matrix, int line, int column, float value );
 int** matrix_internal_create( int sizeX, int sizeY );
 
@@ -17,6 +17,22 @@ int matrix_byte_size( int** mMasterHead, int sizeX, int sizeY );
 int firstElement = 1;
 
 int main () {
+/*  
+    //Teste para Multiplicação
+
+    int **matrix = matrix_create( 5 );
+    matrix_table( matrix, 5, 5 );
+
+    int **matrix2 = matrix_create( 5 );
+    matrix_table( matrix2, 5, 5 );
+
+    int **matrixFinal = matrix_multiply( matrix, matrix2 , 5 , 5, 5 );
+    matrix_table( matrixFinal, 5 , 5 );
+
+    matrix_destroy( matrix, 5);
+    matrix_destroy( matrix2, 5);
+    matrix_destroy( matrixFinal, 5);
+*/
     int **matrix = matrix_internal_create( 5, 9 );
     matrix_internal_add_node(matrix, 2, 3, 99);
     int **matrix2 = matrix_create( 5 );
@@ -66,59 +82,33 @@ int** matrix_create( unsigned int m ) {
 ====================
 matrix_multiply()
 
-    Retorna a multiplicação de duas matrizes introduzidas, considerando que as matrizes tem tamanho igual
+    Retorna a multiplicação de duas matrizes introduzidas
 ====================
 */
 
-/*Matrix* matrix_multiply( Matrix* m, Matrix* n ){
-    int x = 0, tempForSum = 0;
-    int tempX = 0, tempY = 0;
-    Matrix *mTemp, *mHead;
-    Matrix *nTemp, *nHead;
-    for ( nTemp = n->right; nTemp != n; nTemp = nTemp->right, x++ );
-    Matrix *finalMatrix = matrix_internal_create(x, x);
-    mHead = m->below;
-    mTemp = mHead->right;
-    nHead = n->right;
-    nTemp = nHead->below;
-    do{
-        if( nTemp != nHead && mTemp != mHead ){
-            if( mTemp->column == nTemp->line ){
-                tempForSum += mTemp->info * nTemp->info;
-                tempX = mTemp->line;
-                tempY = nTemp->column;
-                mTemp = mTemp->right;
-                nTemp = nTemp->below;
-        }else{
-            if(mTemp->column < nTemp->line){ 
-                mTemp = mTemp->right;
-            }else if ( nTemp->line < mTemp->column ){
-                nTemp = nTemp->below;
-            }else{
-                mTemp = mTemp->right;
-                nTemp = nTemp->below;
-            }
-        }
-        } else {
-            if ( tempForSum != 0 ) {
-                matrix_internal_add_node( finalMatrix ,tempX ,tempY ,tempForSum );
-                tempForSum = 0;
-        }
-        if ( mHead->below == m  && nHead != n) {
-            nHead = nHead->right;
-            nTemp = nHead->below;
-            mHead = m->below;
-            mTemp = mHead->right; 
-        } else {
-            mHead = mHead->below;
-            mTemp = mHead->right;
-            nTemp = nHead->below;
-        }
-    }
-}while( nHead != n );
-return finalMatrix;
-}*/
+int** matrix_multiply( int** m, int** n , int nColluns , int mLinhas , int collumMLineN){
 
+    int somador;
+    int **finalMatrix = matrix_internal_create ( mLinhas , nColluns );
+    int i = 1 ,i2 = 1 ,i3 = 1 ;
+
+    for( i ; i <=  nColluns ; i++ ) {
+        while ( i3 <= mLinhas ) {
+        somador = 0;
+        while (i2 <= collumMLineN ) {
+            somador +=  m[ i3 - 1 ] [ i2 - 1 ] * n[ i2 - 1 ] [ i - 1 ];
+            i2++;
+        }
+
+        matrix_internal_add_node( finalMatrix ,i3 ,i ,somador );
+        i2 = 1;
+        i3++;
+        }
+        i3 = 1;
+    }
+    
+return finalMatrix;
+}
 /*
 ====================
 matrix_create_100mb()
